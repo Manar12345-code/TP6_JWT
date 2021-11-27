aada87772c8c0add9e2386c50eef6130f2761444
Manar12345-code <73133345+Manar12345-code@users.noreply.github.com>
2021-11-27 17:29:05 +0100

    Add files via upload

---
+++ REST_API_JWT-master/src/main/java/com/m/vetements/entities/ProduitProjection.java
@@ -0,0 +1,8 @@
+package com.m.vetements.entities;
+
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "nomVet", types = { Vetement.class })
+public interface ProduitProjection {
+	public String getNomVetement();
+}
---
+++ REST_API_JWT-master/src/main/java/com/m/vetements/security/SecParams.java
@@ -0,0 +1,7 @@
+package com.m.vetements.security;
+
+public interface SecParams {
+	public static final long EXP_TIME = 15*24*60*60*10000;
+	public static final String SECRET = "medberrimaa00";
+	public static final String PREFIX = "Bearer ";
+}
